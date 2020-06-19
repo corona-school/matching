c519645848e89874c094350b7387acdcdc5e220b
Gero <gero.embser@gmail.com>
2020-06-20 00:29:03 +0200

    Refactor main method
    This will refactor the main method by moving the main algorithm execution into a separate function instead of doing all the work in the main function.

---
+++ src/algo.h
@@ -0,0 +1,8 @@
+//
+// Created by Gero Embser on 18.06.20.
+//
+
+#ifndef CORONASCHOOL_MATCHING_ALGO_H
+#define CORONASCHOOL_MATCHING_ALGO_H
+void performAlgorithmWithFilenames(char const *filename_pupils, char const *filename_students, char const *filename_students_lb, char const *filename_dissolved_matches, char const *filename_output);
+#endif //CORONASCHOOL_MATCHING_ALGO_H
